c2bcdc5153ace3a230f3780cb9b333593dcf8f51
Cameron9635 <31054759+Cameron9635@users.noreply.github.com>
2020-02-07 15:22:05 -0600

    Initial Commit

---
+++ HockeyPlayers/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HockeyPlayers.MvcApplication" Language="C#" %>
---
+++ HockeyPlayers/Models/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HockeyPlayers.Models
+{
+    public class Player
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+    }
+}
